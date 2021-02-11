f14621676eca2e968e723c00a74860296abafc2f
Daniel Kirby <dkirby24.10@googlemail.com>
2021-02-11 18:19:31 +0000

    Changed Valorant channel id

--- src/main/java/com/github/kdaniel2410/Constants.java
+++ src/main/java/com/github/kdaniel2410/Constants.java
@@ -8,7 +8,7 @@ public class  Constants {
 
     public static final long VALORANT_TWITTER_ID = 1230550898616586242L;
 
-    public static final long VALORANT_CHANNEL_ID = 1230550898616586242L;
+    public static final long VALORANT_CHANNEL_ID = 805561708809683014L;
 
     public static final long MINECRAFT_TWITTER_ID = 64565898L;
 
