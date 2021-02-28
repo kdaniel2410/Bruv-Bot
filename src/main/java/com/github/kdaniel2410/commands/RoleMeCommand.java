87c2947d51a1e744a67361b270930a8b0198f53f
Daniel Kirby <dkirby24.10@googlemail.com>
2021-02-28 00:48:17 +0000

    Added shortcut to getting all roles to RoleCommand

--- src/main/java/com/github/kdaniel2410/commands/RoleMeCommand.java
+++ src/main/java/com/github/kdaniel2410/commands/RoleMeCommand.java
@@ -8,6 +8,7 @@ import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
+import org.javacord.api.util.logging.ExceptionLogger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +17,16 @@ import java.util.concurrent.CompletableFuture;
 public class RoleMeCommand implements CommandExecutor {
 
     @Command(aliases = {">roleme", ">rm"}, description = "Add or remove roles.", usage = ">roleme <@role>")
-    public void onCommand(Server server, TextChannel channel, User user, Message message) {
+    public void onCommand(String[] args, Server server, TextChannel channel, User user, Message message) {
         if (message.getMentionedRoles().size() < 1) {
             sendMissingRoleMessage(server, channel).thenAccept(response -> message.addMessageDeleteListener(event -> response.delete()));
+        }
+
+        if (args[0].equals("all")) {
+            server.getRoles().forEach(role -> {
+                if (role.getPosition() >= getLowestRolePosition(server)) return;
+                user.addRole(role).exceptionally(ExceptionLogger.get());
+            });
         }
 
         message.getMentionedRoles().forEach(role -> {
@@ -31,7 +39,7 @@ public class RoleMeCommand implements CommandExecutor {
     private CompletableFuture<Message> sendMissingRoleMessage(Server server, TextChannel channel) {
         EmbedBuilder embed = new EmbedBuilder()
                 .setColor(Constants.EMBED_COLOR)
-                .setDescription("Mention the role(s) you would like adding. Here is a list of all the roles I can add or remove:\n\n" + String.join("\n", getLowerRoleTags(server)))
+                .setDescription("Mention the role(s) you would like adding. Hint: you can mention more than one role at once or use ``>roleme all`` to get all available roles. Here is a list of all the roles I can add or remove:\n\n" + String.join("\n", getLowerRoleTags(server)))
                 .setFooter("If you delete your original message, this response will be deleted.");
         return channel.sendMessage(embed);
     }
