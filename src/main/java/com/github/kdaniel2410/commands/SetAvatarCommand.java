48f6c20c04cb8d9f9a9ff10ba1b09bc46e15d599
Daniel Kirby <dkirby24.10@googlemail.com>
2021-03-05 19:15:21 +0000

    Added required permissions to set bot avatar

--- src/main/java/com/github/kdaniel2410/commands/SetAvatarCommand.java
+++ src/main/java/com/github/kdaniel2410/commands/SetAvatarCommand.java
@@ -2,8 +2,10 @@ package com.github.kdaniel2410.commands;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
-import org.javacord.api.AccountUpdater;
 import org.javacord.api.DiscordApi;
+import org.javacord.api.entity.permission.PermissionType;
+import org.javacord.api.entity.server.Server;
+import org.javacord.api.entity.user.User;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,8 +13,9 @@ import java.net.URL;
 public class SetAvatarCommand implements CommandExecutor {
 
     @Command(aliases = {">setavatar"}, usage = ">setavatar <url>")
-    public String onCommand(String[] args, DiscordApi api) {
+    public String onCommand(String[] args, DiscordApi api, User user, Server server) {
         if (args.length < 1) return "**Error** not enough arguments";
+        if (!server.hasPermission(user, PermissionType.MANAGE_SERVER)) return "**Error** you do not have the required permissions to do that";
         try {
             api.updateAvatar(new URL(args[0]));
         } catch (MalformedURLException e) {
